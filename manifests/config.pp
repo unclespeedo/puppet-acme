c607628eb0eb437efda6215fd62c286365671295
Ryan Speed <rspeed@speedo.ca>
2016-08-11 11:55:50 -0700

    back to system pyton

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,7 +17,7 @@ class acme::config(
   } ->
   python::virtualenv { $pip_home:
     ensure       => present,
-    version      => '3',
+    version      => 'system',
     venv_dir     => $pip_home,
     owner        => $user,
     group        => $group,
