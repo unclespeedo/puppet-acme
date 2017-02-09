e314a60b915eda5738f11e89c3e661d806690d3a
Ryan Speed <rspeed@speedo.ca>
2017-02-09 15:21:41 -0800

    remove comma on line 2 and re-add environment varialbe sfor dns

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class acme::params {
-  $userhome          = "/home/$user",
+  $userhome          = "/home/$user"
   $acme_home         = "$userhome/.acme.sh"
   $pip_home          = "$userhome/pip"
   $pip_bin           = "$pip_home/bin"
@@ -7,7 +7,7 @@ class acme::params {
   $package_name      = 'acme.sh'
   $package_ensure    = 'latest'
   $repo_manage       = false
-  $repo_location     = ""
+  $repo_location     = ''
   $repo_trusted      = false
   $user              = 'acme'
   $group             = 'acme'
@@ -16,5 +16,9 @@ class acme::params {
   $cert              = "$certhome/$::fqdn/fullchain.cer"
   $working_dir       = '/usr/share/acme.sh'
   $issue_command     = "acme.sh --home $home --issue -d $::fqdn --dns dns_lexicon --debug 2 >> $userhome/renew.log 2>&1"
-  $dns_environment   = ""
+  $dns_environment   = {
+    PROVIDER                     => 'dnsmadeeasy',
+    LEXICON_DNSMADEEASY_USERNAME => '',
+    LEXICON_DNSMADEEASY_TOKEN    => ''
+  }
 }
