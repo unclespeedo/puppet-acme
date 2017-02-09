a9de21c1f398e339aaf25d5fa2aefb2a57adb45d
Ryan Speed <rspeed@speedo.ca>
2017-02-09 15:13:41 -0800

    last try

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,8 +17,8 @@ class acme::params {
   $working_dir       = '/usr/share/acme.sh'
   $issue_command     = "acme.sh --home $home --issue -d $::fqdn --dns dns_lexicon --debug 2 >> $userhome/renew.log 2>&1"
   $dns_environment   = [
-    PROVIDER                     => 'dnsmadeeasy',
-    LEXICON_DNSMADEEASY_USERNAME => '',
-    LEXICON_DNSMADEEASY_TOKEN    => ''
+    'PROVIDER'                     => 'dnsmadeeasy',
+    'LEXICON_DNSMADEEASY_USERNAME' => '',
+    'LEXICON_DNSMADEEASY_TOKEN'    => ''
   ]
 }
