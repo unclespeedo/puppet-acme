873427ea47ddc998f3408c583935208e318b5974
Ryan Speed <rspeed@speedo.ca>
2017-02-09 15:35:44 -0800

    move installation to base class

--- manifests/init.pp
+++ manifests/init.pp
@@ -32,7 +32,20 @@ class acme (
   validate_array($dns_environment)
   
   if $package_manage {
-    include acme::install
+    if $repo_manage {
+      apt::source { $package_name:
+      location        => "$repo_location",
+        release         => 'trusty',
+        repos           => 'main',
+        allow_unsigned  => $repo_trusted,
+      }
+    }
+  package { $package_name:
+    ensure   => $package_ensure,
+    notify   => Exec['acme-install'],
+  }
+  Class['apt::update'] -> Package[$package_name]
+
   }
   
   include acme::config
