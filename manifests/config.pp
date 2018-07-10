c4c19ac0061f4b13739ebd183ceeecf0ab9e9180
Ryan Speed <rspeed@speedo.ca>
2018-07-10 11:34:04 -0700

    fix all lint errors (whitespace, variable definitions, etc)

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,53 +7,52 @@ class acme::config inherits acme {
     dev        => 'absent',
     virtualenv => 'present',
     gunicorn   => 'absent',
-  } ->
-  python::virtualenv { $pip_home:
-    ensure       => present,
-    version      => 'system',
-    venv_dir     => $pip_home,
-    owner        => $user,
-    group        => $group,
-  } ->
-  python::pip { 'dns-lexicon' :
-    pkgname       => 'dns-lexicon',
-    virtualenv    => $pip_home,
-    ensure        => '1.1.8',
-    owner         => $user,
-    group         => $group,
-  } ->
-  exec { "acme-install":
-    cwd     => $working_dir,
-    command => $install_command,
-    path    => [ $working_dir, $pip_bin, '/bin', '/usr/bin' ],
-    user    => $user,
-    creates => $acme_home,
-    require => User[$user],
   }
-  file { $cert_home:
-    ensure    => directory,
-    owner     => $user,
-    mode      => '700',
-    require   => Exec['acme-install'],
+  -> python::virtualenv { $::pip_home:
+    ensure   => present,
+    version  => 'system',
+    venv_dir => $::pip_home,
+    owner    => $::user,
+    group    => $::group,
   }
-  exec { "acme-issue":
-    cwd          => $user_home,
-    environment  => $dns_environment,
-    command      => "acme.sh --home $acme_home --issue $issue_command >> $user_home/renew.log 2>&1",
-    user         => $user,
-    path         => [$acme_home, $pip_bin, '/bin', '/usr/bin' ],
-    creates      => $cert,
-    require      => File[$cert_home],
+  -> python::pip { 'dns-lexicon' :
+    ensure     => '1.1.8',
+    pkgname    => 'dns-lexicon',
+    virtualenv => $::pip_home,
+    owner      => $::user,
+    group      => $::group,
   }
-  
+  -> exec { 'acme-install':
+    cwd     => $::working_dir,
+    command => $::install_command,
+    path    => [ $::working_dir, $::pip_bin, '/bin', '/usr/bin' ],
+    user    => $::user,
+    creates => $::acme_home,
+    require => User[$::user],
+  }
+  file { $::cert_home:
+    ensure  => directory,
+    owner   => $::user,
+    mode    => '0700',
+    require => Exec['acme-install'],
+  }
+  exec { 'acme-issue':
+    cwd         => $::user_home,
+    environment => $::dns_environment,
+    command     => "acme.sh --home ${::acme_home} --issue ${::issue_command} >> ${::user_home}/renew.log 2>&1",
+    user        => $::user,
+    path        => [$::acme_home, $::pip_bin, '/bin', '/usr/bin' ],
+    creates     => $::cert,
+    require     => File[$::cert_home],
+  }
   cron { 'Renew SSL Cert':
-    ensure       => present,
-    environment  => "MAILTO=$accountemail",
-    command      => "$acme_home/acme.sh --cron --home $acme_home >> $user_home/renew.log 2>&1",
-    hour         => "4",
-    minute       => "35",
-    user         => $user,
-    require      => Exec['acme-install'],
+    ensure      => present,
+    environment => "MAILTO=${::accountemail}",
+    command     => "${::acme_home}/acme.sh --cron --home ${::acme_home} >> ${::user_home}/renew.log 2>&1",
+    hour        => '4',
+    minute      => '35',
+    user        => $::user,
+    require     => Exec['acme-install'],
   }
 
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -22,24 +22,22 @@ class acme (
   Optional[Stdlib::Absolutepath] $working_dir,
   Optional[Array[String]] $dns_environment,
 )  {
-  
   if $package_manage {
     if $repo_manage {
       apt::source { $package_name:
-      location        => "$repo_location",
-        release         => 'trusty',
-        repos           => 'main',
-        allow_unsigned  => $repo_trusted,
+        location       => $repo_location,
+        release        => 'trusty',
+        repos          => 'main',
+        allow_unsigned => $repo_trusted,
       }
     }
   package { $package_name:
-    ensure   => $package_ensure,
-    notify   => Exec['acme-install'],
+    ensure => $package_ensure,
+    notify => Exec['acme-install'],
   }
   Class['apt::update'] -> Package[$package_name]
 
   }
-  
   include acme::config
 
 }
